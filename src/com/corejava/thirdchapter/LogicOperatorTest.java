6ae48d465c06a71dcbdd12998feac3093ea5a7d7
alone <1584060557@qq.com>
2020-04-18 09:57:17 +0800

    commit

---
+++ src/com/corejava/thirdchapter/LogicOperatorTest.java
@@ -0,0 +1,8 @@
+package com.corejava.thirdchapter;
+
+public class LogicOperatorTest {
+    public static void main(String[] args) {
+        System.out.println(20 & 2);
+        System.out.println(20 << 1);
+    }
+}
